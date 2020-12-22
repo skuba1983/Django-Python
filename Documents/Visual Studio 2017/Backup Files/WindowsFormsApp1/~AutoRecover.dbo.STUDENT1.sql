508d7437600c8aecef3c564d217f7e3fa52c6134
skuba1983 <skubaav@gmail.com>
2020-12-22 02:43:37 +0200

    C:/Users/skuba/decompiler
    Signed-off-by: skuba1983 <skubaav@gmail.com>

---
+++ Documents/Visual Studio 2017/Backup Files/WindowsFormsApp1/~AutoRecover.dbo.STUDENT1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[STUDENT1] (
+    [STUDENT_ID]         INT              NOT NULL,
+    [SURNAME]    NCHAR(25) NOT NULL DEFAULT NEWID(),
+    [NAME]       NCHAR (10)       NOT NULL,
+    [STIPEND]    INT              NOT NULL,
+    [KURS]       INT NOT NULL DEFAULT NEWID(),
+    [CITY]       NCHAR (15)       NOT NULL,
+    [BITHDAY]    DATE             NOT NULL,
+    [UNIV-ID]    INT              NULL,
+	CHECK (STIPEND>=5000),
+    PRIMARY KEY CLUSTERED ([STUDENT_ID] ASC)
+);
+
