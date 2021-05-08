9e5fba9fde7c9ec79e706c8085e4e913ddf7dcdd
skuba1983 <skubaav@gmail.com>
2021-05-08 10:33:13 +0300

    AAAA

---
+++ source/repos/Django1/Django1/Django1/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for Django1.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', Django1.views.home, name='home'),
+    # url(r'^Django1/', include('Django1.Django1.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
